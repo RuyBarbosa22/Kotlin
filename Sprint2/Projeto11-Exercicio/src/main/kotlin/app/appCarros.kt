e0ef368864852668aa4fddad82acb74aaed85c52
RuyBarbosa22 <ruy.castro@sptech.school>
2022-10-04 21:22:06 -0300

    Finalizando exercicio de prática de Querys no Kotlin

--- Sprint2/Projeto11-Exercicio/src/main/kotlin/app/appCarros.kt
+++ Sprint2/Projeto11-Exercicio/src/main/kotlin/app/appCarros.kt
@@ -12,7 +12,7 @@ fun main() {
 
     var contador = showInputDialog("Quantos carros deseja inserir?").toInt()
 
-    while (contador > 0){
+    while (contador > 0) {
 
         val novoModelo = showInputDialog("Nome do carro")
         val novoAno = showInputDialog("Ano").toInt()
@@ -23,37 +23,51 @@ fun main() {
 
         contador--
     }
-        val carros = carrosRepository.Listar()
-        var consulta = "${contador} Carros foram cadastrados:  \r\n\r\n"
+    val carros = carrosRepository.Listar()
+    var consulta = "${contador} Carros foram cadastrados:  \r\n\r\n"
 
 
-        carros.forEach {
-            consulta += "ID: ${it.id}, Modelo: ${it.modelo}, Ano: ${it.ano}, Potência: ${it.potencia}"
-            consulta += "\r\n"
-        }
-        showMessageDialog(null, consulta)
+    carros.forEach {
+        consulta += "ID: ${it.id}, Modelo: ${it.modelo}, Ano: ${it.ano}, Potência: ${it.potencia}"
+        consulta += "\r\n"
+    }
+    showMessageDialog(null, consulta)
 
 
     val rever = showInputDialog("Qual carro deseja rever? Digite um ID válido").toInt()
-    if (carrosRepository.selectEspecifico(rever) == true) {
-            val Rcarro = carrosRepository.select(rever)
-        showMessageDialog(null, "ID: ${Rcarro.id}, modelo: ${Rcarro.modelo}, ano: ${Rcarro.ano}, potência: ${Rcarro.potencia}")
-    }else{
-        showMessageDialog(null,"Carro de ID ${rever} não encontrado")
+    if (carrosRepository.selectEspecifico(rever)) {
+        val Rcarro = carrosRepository.select(rever)
+        showMessageDialog(
+            null,
+            "ID: ${Rcarro.id}, modelo: ${Rcarro.modelo}, ano: ${Rcarro.ano}, potência: ${Rcarro.potencia}"
+        )
+    } else {
+        showMessageDialog(null, "Carro de ID ${rever} não encontrado")
     }
 
-    showMessageDialog(null, carrosRepository.selectOrdenado())
+    val seminovos = CarrosRepository(jdbcTemplate).listarSeminovos()
 
+    var mensagemSeminovos = "${seminovos.size} carros seminovos: \r\n"
+    seminovos.forEach {
+        mensagemSeminovos += "id: ${it.id} - ${it.modelo}, ano ${it.ano}, potência de ${it.potencia} \r\n"
+    }
+    showMessageDialog(null, mensagemSeminovos)
 
-//    val idConsulta = showInputDialog("Id para consultar").toInt()
-//    val carroEncontrado = carrosRepository.recuperar(idConsulta)
-//    showMessageDialog(null, carroEncontrado)
-//
-//    val idExclusao = showInputDialog("Id para excluir").toInt()
-//    val excluidos = carrosRepository.excluir(idExclusao)
-//    if (excluidos >0) {
-//        showMessageDialog(null,"Carro $idExclusao excluido!")
-//    }else {
-//        showMessageDialog(null, "Carro não encontrado: (")
-//    }
+    val seminovosPotentes = CarrosRepository(jdbcTemplate).listarSeminovosPotentes()
+
+    var mensagemPotentes = "${seminovosPotentes.size} carros seminovos: \r\n"
+    seminovosPotentes.forEach {
+        mensagemPotentes += "id: ${it.id} - ${it.modelo}, ano ${it.ano}, potência de ${it.potencia} \r\n"
+    }
+    showMessageDialog(null, mensagemPotentes)
+
+    val modeloExcluir = showInputDialog(
+        "Qual o modelo de carro que deseja excluir do banco?")
+
+    val excluidos = CarrosRepository(jdbcTemplate).excluirPorModelo(modeloExcluir)
+    if (excluidos > 0) {
+        showMessageDialog(null, "$excluidos carros excluídos")
+    } else {
+        showMessageDialog(null, "Nenhum carro excluído")
+    }
 }
--- Sprint2/Projeto11-Exercicio/src/main/kotlin/repositorio/CarrosRepository.kt
+++ Sprint2/Projeto11-Exercicio/src/main/kotlin/repositorio/CarrosRepository.kt
@@ -8,47 +8,62 @@ import java.beans.BeanProperty
 
 class CarrosRepository(val jdbcTemplate: JdbcTemplate) {
 
-    fun inserir(carro: Carro){
-        jdbcTemplate.update("""
+    fun inserir(carro: Carro) {
+        jdbcTemplate.update(
+            """
             insert into Carro (modelo,ano,potencia) values
             (?,?,?)
-        """, carro.modelo, carro.ano, carro.potencia)
+        """, carro.modelo, carro.ano, carro.potencia
+        )
     }
 
-    fun Listar():List<Carro> {
-        return jdbcTemplate.query("select * from Carro",
-            BeanPropertyRowMapper(Carro::class.java))
+    fun Listar(): List<Carro> {
+        return jdbcTemplate.query(
+            "select * from Carro",
+            BeanPropertyRowMapper(Carro::class.java)
+        )
     }
 
-    fun selectEspecifico(rever:Int):Boolean {
+    fun selectEspecifico(rever: Int): Boolean {
         val carro = jdbcTemplate.queryForObject(
             "select * from Carro where id = ?",  // "?" será substituido pelo id
-            BeanPropertyRowMapper(Carro::class.java),rever)
+            BeanPropertyRowMapper(Carro::class.java), rever
+        )
         return carro != null
     }
 
-        fun select(rever:Int):Carro {
-            val select = jdbcTemplate.queryForObject(
-                "select * from Carro where id = ?",  // "?" será substituido pelo id
-                BeanPropertyRowMapper(Carro::class.java), rever
-            )
-            return select
-        }
-
-
-    fun selectOrdenado():Carro {
+    fun select(rever: Int): Carro {
         val select = jdbcTemplate.queryForObject(
-            "select * from Carro where ano >= 2019 order by potencia",
-            BeanPropertyRowMapper(Carro::class.java)
+            "select * from Carro where id = ?",  // "?" será substituido pelo id
+            BeanPropertyRowMapper(Carro::class.java), rever
         )
         return select
     }
 
-    fun selectOrdenado2():Carro {
-        val select = jdbcTemplate.queryForObject(
-            "select * from Carro where ano >= 2019 and potencia > 1.5, order by potencia",
+
+    fun listarSeminovos(): List<Carro> {
+        return jdbcTemplate.query(
+            "select * from carro where ano >= 2019 order by potencia",
             BeanPropertyRowMapper(Carro::class.java)
         )
-        return select
+    }
+
+    fun listarSeminovosPotentes(): List<Carro> {
+        return jdbcTemplate.query(
+            """
+        select * from carro 
+        where ano >= 2019 and potencia >= 1.5 
+        order by ano            
+        """,
+            BeanPropertyRowMapper(Carro::class.java)
+        )
+    }
+
+    fun excluirPorModelo(modelo: String): Int {
+        val excluidos = jdbcTemplate.update(
+            "delete from carro where modelo like ?",
+            "%$modelo%"
+        )
+        return excluidos
     }
 }
