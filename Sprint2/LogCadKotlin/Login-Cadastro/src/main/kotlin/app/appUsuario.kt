988d18a1941f2b1a0fee34d86b30c22a1d6a9dec
RuyBarbosa22 <ruy.castro@sptech.school>
2022-10-06 14:42:36 -0300

    Trabalhando no projeto LogCadKotlin

--- Sprint2/LogCadKotlin/Login-Cadastro/src/main/kotlin/app/appUsuario.kt
+++ Sprint2/LogCadKotlin/Login-Cadastro/src/main/kotlin/app/appUsuario.kt
@@ -1,19 +1,29 @@
 package app
 
+import dominio.Usuario
+import repositorio.UsuarioRepository
 import javax.swing.JOptionPane
 import javax.swing.JOptionPane.*
 
 fun main() {
     while (true) {
 
-        val resposta1 = showInputDialog("Bem vindo! Acesse sua conta, ou crie uma. \r\n 1 - Cadastro \r\n 2 - Entrar").toInt()
+        val resposta1 = showInputDialog("Bem vindo! Acesse sua conta, ou crie uma. \r\n 1 - Cadastro \r\n 2 - Entrar \r\n 3 - Sair").toInt()
 
         if (resposta1 == 1){
-
+            var nomeCad = showInputDialog("Nome de usuário").also { Usuario().nome = it }
+            var emailCad = showInputDialog("Email").also { Usuario().email = it }
+            var telCad = showInputDialog("Telefone").also { Usuario().tel = it }
+            var senhaCad = showInputDialog("Senha de acesso").also { Usuario().senha = it }
+            var senha2Cad = showInputDialog("Confirme a senha")
+            if (senhaCad != senha2Cad){
+                showMessageDialog(null,"Senhas diferentes!")
+            }
         } else if (resposta1 == 2){
 
         } else {
-            showMessageDialog(null, "Respota inválida!")
+            showMessageDialog(null, "Saindo...")
+            break
         }
     }
 }
