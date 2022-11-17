7c3f24db4073f7401906a721458145da3e577fd2
RuyBarbosa22 <ruy.castro@sptech.school>
2022-11-17 12:17:46 -0300

    Trabalhando na API de Kotlin

--- Sprint3/Projeto - LogCad/src/main/kotlin/app/appUsuario.kt
+++ Sprint3/Projeto - LogCad/src/main/kotlin/app/appUsuario.kt
@@ -1,10 +1,13 @@
 package app
 
 import com.github.britooo.looca.api.core.Looca
+import com.microsoft.sqlserver.jdbc.StringUtils.isNumeric
 import config.Conexao
 import dominio.Empresa
 import repositorio.EmpresaRepository
+import java.util.*
 import javax.swing.JOptionPane.*
+import kotlin.concurrent.schedule
 
 open class Main {
     companion object {
@@ -116,11 +119,13 @@ open class Main {
                 } else {
                     empresa.senha = senhaCad
                     empresa.codEmpresa = randomString
-                    showMessageDialog(null,"""
+                    showMessageDialog(
+                        null, """
                         Cadastro realizado com sucesso!
                         Esse é seu código empresarial: ${randomString}
                         Guarde-o pois é necessário para demais funcionalidades
-                    """.trimIndent())
+                    """.trimIndent()
+                    )
                     EmpresaRepository(jdbcTemplate).cadastro(empresa)
                     showMessageDialog(null, "Cadastro realizado com sucesso!")
                     showMessageDialog(null, "Redirecionando...")
@@ -147,20 +152,18 @@ open class Main {
                     while (true) {
                         val resp = showInputDialog(
                             """
-                        Oque deseja monitorar?
-                        1 - CPU
-                        2 - RAM
-                        3 - Disco
-                        4 - Sobre o computador
-                        5 - Sair
+                        Oque deseja fazer?
+                        1 - Monitorar Hardware
+                        2 - Cadastrar usuários
+                        3 - Cadastrar Máquinas
+                        4 - Logout
             """.trimIndent()
                         )
 
                         when (resp) {
-                            "1" -> monitorarCPU()
-                            "2" -> monitorarRAM()
-                            "3" -> monitorarHD()
-                            "4" -> monitorarPC()
+                            "1" -> monitorar()
+                           // "2" -> monitorarRAM()
+                           // "3" -> monitorarHD()
                             else -> return
                         }
                     }
@@ -172,12 +175,69 @@ open class Main {
             }
         }
 
-            fun getRandomString(length: Int) : String {
-                val charset = "0123456789"
-                return (1..length)
-                    .map { charset.random() }
-                    .joinToString("")
+        fun getRandomString(length: Int): String {
+            val charset = "0123456789"
+            return (1..length)
+                .map { charset.random() }
+                .joinToString("")
+        }
+
+        fun monitorar() {
+
+            while (true) {
+                var resp = showInputDialog(
+                    """
+                Unidade de medida de tempo:
+                1 - segundos
+                2 - minutos
+                3 - horas
+            """.trimIndent()
+                )
+
+                while (true) {
+                    if (resp == "1") {
+                        resp = "segundos"
+                        break
+                    } else if (resp == "2") {
+                        resp = "minutos"
+                        break
+                    } else if (resp == "3") {
+                        resp = "horas"
+                        break
+                    } else {
+                        showMessageDialog(null, "Opção inválida!")
+                    }
+                }
+
+
+                var resp2 = showInputDialog(
+                    """
+                        De quanto em quanto ${resp} você deseja gravar
+                        os dados das máquinas?
+                    """.trimIndent()
+                ).toLong()
+
+
+                while (true) {
+                    if (resp == "segundos") {
+                        resp2 = resp2 * 1000
+                    } else if (resp == "minutos") {
+                        resp2 = (resp2 * 60) *1000
+                    } else if (!isNumeric(resp2.toString())) {
+                        showMessageDialog(null, "Valor inválido!")
+                    } else {
+                        resp2 = ((resp2 * 60) *60) * 1000
+                    }
+                }
+
+
+                Timer().schedule(resp2) {
+
+                }
             }
+
+        }
+
 
         fun monitorarCPU() {
             val looca = Looca()
@@ -189,53 +249,55 @@ open class Main {
         Modelo: ${processador.nome}
         Frequência (GHz): ${processador.frequencia.toDouble() / 1024 / 1024 / 1024}
         Uso (%): ${processador.uso}
-    """)
-        }
-
-        fun monitorarRAM() {
-            val looca = Looca()
-
-            val memoria = looca.memoria
-
-            showMessageDialog(
-                null, """
-        Total (GB): ${"%.2f".format(memoria.total.toDouble() / 1024 / 1024 / 1024)}
-        Em uso (GB): ${"%.2f".format(memoria.emUso.toDouble() / 1024 / 1024 / 1024)}
-        Disponível (GB): ${"%.2f".format(memoria.disponivel.toDouble() / 1024 / 1024 / 1024)}
-    """)
-        }
-
-        fun monitorarHD() {
-            val looca = Looca()
-
-            val grupoDiscos = looca.grupoDeDiscos
-
-            val discos = grupoDiscos.discos
-            discos.forEachIndexed { d, disco ->
-                showMessageDialog(
-                    null, """
-            Disco ${d + 1}:
-            Modelo: ${disco.modelo}
-            Tamanho (GB): ${disco.tamanho.toDouble() / 1024 / 1024 / 1024}
-        """
-                )
-            }
-        }
-
-        fun monitorarPC() {
-            val looca = Looca()
-            val sistema = looca.sistema
-
-            showMessageDialog(
-                null, """
-        Fabricante: """ + sistema.fabricante + """
-        Arquitetura: """ + sistema.arquitetura + """
-        SO: """ + sistema.sistemaOperacional + """
-        Ligado há: """ + sistema.tempoDeAtividade + """ horas
-        Ligado desde: """ + sistema.inicializado + """
     """
             )
         }
+//
+//        fun monitorarRAM() {
+//            val looca = Looca()
+//
+//            val memoria = looca.memoria
+//
+//            showMessageDialog(
+//                null, """
+//        Total (GB): ${"%.2f".format(memoria.total.toDouble() / 1024 / 1024 / 1024)}
+//        Em uso (GB): ${"%.2f".format(memoria.emUso.toDouble() / 1024 / 1024 / 1024)}
+//        Disponível (GB): ${"%.2f".format(memoria.disponivel.toDouble() / 1024 / 1024 / 1024)}
+//    """
+//            )
+//        }
+//
+//        fun monitorarHD() {
+//            val looca = Looca()
+//
+//            val grupoDiscos = looca.grupoDeDiscos
+//
+//            val discos = grupoDiscos.discos
+//            discos.forEachIndexed { d, disco ->
+//                showMessageDialog(
+//                    null, """
+//            Disco ${d + 1}:
+//            Modelo: ${disco.modelo}
+//            Tamanho (GB): ${disco.tamanho.toDouble() / 1024 / 1024 / 1024}
+//        """
+//                )
+//            }
+//        }
+//
+//        fun monitorarPC() {
+//            val looca = Looca()
+//            val sistema = looca.sistema
+//
+//            showMessageDialog(
+//                null, """
+//        Fabricante: """ + sistema.fabricante + """
+//        Arquitetura: """ + sistema.arquitetura + """
+//        SO: """ + sistema.sistemaOperacional + """
+//        Ligado há: """ + sistema.tempoDeAtividade + """ horas
+//        Ligado desde: """ + sistema.inicializado + """
+//    """
+//            )
+//        }
 
 
     }
